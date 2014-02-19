c541f91ddc24b9d0896cc6b538f2bbd5854fd53a
Da-Huang <dhuang.cn@gmail.com>
2014-02-19 19:28:19 +0800

    DataBase

--- Cloud-Disk-Search/src/database/DBConnection.java
+++ Cloud-Disk-Search/src/database/DBConnection.java
@@ -1,5 +1,27 @@
 package database;
 
-public class DBConnection {
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
 
+import org.apache.commons.dbcp.BasicDataSource;
+
+public class DBConnection {
+	static private BasicDataSource ds = null;
+	private DBConnection() {}
+	
+	public static Connection getConnection() throws SQLException {
+		if ( ds == null ) {
+			ds = new BasicDataSource();
+			try {
+				Properties prop = new Properties();
+				prop.load(DBConnection.class.getClassLoader().
+						getResource("conf.properties").openStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return ds.getConnection();
+	}
 }

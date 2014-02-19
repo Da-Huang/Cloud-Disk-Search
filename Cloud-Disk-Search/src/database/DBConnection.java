30d9fd12cf40f8c61f1feb5a244255eb856cf005
Da-Huang <dhuang.cn@gmail.com>
2014-02-19 19:54:14 +0800

    DataBase Connecting Pool Finished.

--- Cloud-Disk-Search/src/database/DBConnection.java
+++ Cloud-Disk-Search/src/database/DBConnection.java
@@ -2,14 +2,34 @@ package database;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 import org.apache.commons.dbcp.BasicDataSource;
 
+
+/**
+ * DataBase Connecting Pool
+ * @author dhuang
+ *
+ */
 public class DBConnection {
 	static private BasicDataSource ds = null;
 	private DBConnection() {}
+	
+	public static void main(String[] args) throws SQLException {
+		Connection conn = DBConnection.getConnection();
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM `files`");
+		while ( rs.next() ) {
+			System.out.println(rs.getString(1));
+		}
+		rs.close();
+		stmt.close();
+		conn.close();
+	}
 	
 	public static Connection getConnection() throws SQLException {
 		if ( ds == null ) {
@@ -18,6 +38,11 @@ public class DBConnection {
 				Properties prop = new Properties();
 				prop.load(DBConnection.class.getClassLoader().
 						getResource("conf.properties").openStream());
+				ds.setMaxIdle(Integer.parseInt(prop.getProperty("maxIdle")));
+				ds.setMaxActive(Integer.parseInt(prop.getProperty("maxActive")));
+				ds.setUrl(prop.getProperty("url"));
+				ds.setDriverClassName(prop.getProperty("driverClassName"));
+				
 			} catch (IOException e) {
 				e.printStackTrace();
 			}

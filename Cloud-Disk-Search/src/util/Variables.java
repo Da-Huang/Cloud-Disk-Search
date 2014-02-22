722d3bceac526aff8e22773f939f595f5dfd435a
Da-Huang <dhuang.cn@gmail.com>
2014-02-22 09:42:59 +0800

    Change conf.prop

--- Cloud-Disk-Search/src/util/Variables.java
+++ Cloud-Disk-Search/src/util/Variables.java
@@ -1,16 +1,16 @@
 package util;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-
-import database.DBConnection;
 
 
 public class Variables {
 	private Variables() {
 		try {
-			properties.load(DBConnection.class.getClassLoader().
-					getResource("conf.properties").openStream());
+			properties.load(new FileInputStream("conf.properties"));
+//			properties.load(Variables.class.getClassLoader().
+//					getResource("conf.properties").openStream());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -19,6 +19,11 @@ public class Variables {
 	public static Variables getInstance() {
 		if ( INSTANCE == null ) INSTANCE = new Variables();
 		return INSTANCE;
+	}
+	
+	public static void main(String[] args) {
+		Variables.getInstance();
+		System.out.println(Variables.getInstance().getProperties().getProperty("indexPath"));
 	}
 	
 	private Properties properties = new Properties();

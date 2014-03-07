fdf66b6d16c3c633d1d4977124738ad401b39890
Da-Huang <dhuang.cn@gmail.com>
2014-03-07 09:36:19 +0800

    AppException

--- Cloud-Disk-Search/src/exception/AppException.java
+++ Cloud-Disk-Search/src/exception/AppException.java
@@ -1,6 +1,10 @@
 package exception;
 
-
+/**
+ * App Excetion.
+ * @author dhuang
+ *
+ */
 public class AppException extends Exception {
 	private static final long serialVersionUID = 7696261877362832852L;
 	public AppException(String message) {

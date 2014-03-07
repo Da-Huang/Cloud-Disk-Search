f613a37f8911fe007246e0f74d7f69663bf854b7
Da-Huang <dhuang.cn@gmail.com>
2014-03-07 09:34:29 +0800

    Add AppException and hot list.

--- Cloud-Disk-Search/src/tcp/TCPWorker.java
+++ Cloud-Disk-Search/src/tcp/TCPWorker.java
@@ -19,6 +19,7 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.FSDirectory;
 
+import exception.AppException;
 import util.Variables;
 
 
@@ -34,30 +35,56 @@ public class TCPWorker implements Runnable {
 	@Override
 	public void run() {
 		logger.entry(client);
+		IndexReader reader = null;
+		BufferedReader br = null;
 		try {
-			IndexReader reader = DirectoryReader.open(FSDirectory.open(
+			reader = DirectoryReader.open(FSDirectory.open(
 					new File(Variables.getInstance().getProperties().getProperty("indexPath"))));
 			IndexSearcher searcher = new IndexSearcher(reader);
 			
-			BufferedReader br = new BufferedReader(
+			br = new BufferedReader(
 					new InputStreamReader(client.getInputStream()));
 			String request = br.readLine();
 			logger.info("request: " + request);
 			if ( request != null ) {
 				JSONObject jin = JSONObject.fromObject(request.trim());
-				String query = jin.getString("query").trim();
-				int start = jin.getInt("start");
-				int limit  = jin.getInt("limit");
+				String type = jin.getString("type");
+				if ( type == null ) throw new AppException("No Type.");
+				type = type.trim();
 				
-				JSONObject jout = Searcher.getInstance().search(searcher, 
-						QueryParser.getInstance().parseAsField(query, "name"), start, limit);
-				BufferedWriter bw = new BufferedWriter(
-						new OutputStreamWriter(client.getOutputStream()));
-				bw.write(jout.toString());
-				bw.close();
+				if ( type.equals("search") ) {
+					String query = jin.getString("query");
+					int start = jin.getInt("start");
+					int limit  = jin.getInt("limit");
+					
+					JSONObject jout = Searcher.getInstance().search(searcher, 
+							QueryParser.getInstance().parseAsField(query, "name"), start, limit);
+					BufferedWriter bw = new BufferedWriter(
+							new OutputStreamWriter(client.getOutputStream()));
+					bw.write(jout.toString());
+					bw.close();
+				} else if ( type.equals("hot") ) {
+					JSONObject jout = Searcher.getInstance().search(searcher, 
+							QueryParser.getInstance().parseAsField("mp4", "name"), 0, 100);
+					BufferedWriter bw = new BufferedWriter(
+							new OutputStreamWriter(client.getOutputStream()));
+					bw.write(jout.toString());
+					bw.close();
+					
+				} else throw new AppException("Type Error.");
 			}
 			br.close();
+			reader.close();
 			client.close();
+		} catch (AppException e) {
+			try {
+				br.close();
+				reader.close();
+				client.close();
+			} catch (IOException ioe) {
+				logger.error(ioe);
+			}
+			logger.error(e);
 		} catch (IOException e) {
 			logger.error("Client Error.");
 		}

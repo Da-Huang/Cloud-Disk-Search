95114bc83f7f2dc90505fe6ccb4baf044068f745
Da Huang <dhuang.cn@gmail.com>
2014-10-11 08:14:03 +0800

    configure: increase threadnum

---
+++ src/test/TestSetStatus.java
@@ -0,0 +1,9 @@
+package test;
+
+import crawl.yun.util.UserSet;
+
+public class TestSetStatus {
+  public static void main(String[] args) {
+    UserSet.getInstance().setStatus(656323722L, "running1");
+  }
+}
